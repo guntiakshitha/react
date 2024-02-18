a5681e07790c9df4f9045b75644a64158cfea77d
guntiakshitha <akshitha.g@sageit.in>
2024-02-18 18:45:02 +0530

    Basics1

---
+++ App.js
@@ -0,0 +1,5 @@
+
+    const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, React.createElement("h1", {},"I am h1 tag")));
+    console.log(parent)
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent)  
